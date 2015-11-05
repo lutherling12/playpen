e1163670724ea77d31a9d1aed1a8402e4619a9af
Luther <lutherling12@gmail.com>
2015-11-04 21:35:59 -0500

    Builds, but server fails to connect.

---
+++ sockets/chatClient.cpp
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <pthread.h>
+
+#define CHAR_LIMIT 2048
+
+int main (int argc, char * argv[])
+{
+  const char* ip = "192.168.0.13";
+  const char* port = "50008";
+
+  struct addrinfo config;
+    memset (&config, 0, sizeof(struct addrinfo));
+    config.ai_family = AF_UNSPEC;
+    config.ai_socktype = SOCK_STREAM;
+    config.ai_flags = 0;
+    config.ai_protocol = 0;
+
+  struct addrinfo* results;
+
+  int gai = getaddrinfo (ip, port, &config, &results);
+  if (gai != 0) {
+    fprintf (stderr, "getaddrinfo: %s\n", gai_strerror(gai));
+    exit (EXIT_FAILURE);
+  }
+
+  int sfd = socket (
+    results->ai_family, 
+    results->ai_socktype, 
+    results->ai_protocol
+  );
+
+  if (sfd == -1) {
+    fprintf (stderr, "Socket not open.\n");
+    exit (EXIT_FAILURE);
+  }
+
+  if (connect (sfd, results->ai_addr, results->ai_addrlen) != 0) {
+    fprintf (stderr, "Failed to connect.\n");
+    exit (EXIT_FAILURE);
+  }
+
+  freeaddrinfo (results);
+
+  write (sfd, "Hello", sizeof("Hello"));
+
+  close (sfd);
+  return 0;
+}
